cee3d51d127359f5a641fa58f6eaf9f348909945
rgalarpe <46391075+rgalarpe@users.noreply.github.com>
2026-02-02 16:33:41 +0000

    project completion

--- practice1/Donate-App/src/pages/Donation/Donate-Page.jsx
+++ practice1/Donate-App/src/pages/Donation/Donate-Page.jsx
@@ -4,93 +4,96 @@ import { useState } from "react";
 
 
 export default function DonatePage() {
-    const { donate, showToast, balance } = useAppContext();
+  const {donate, showToast, balance} = useAppContext();
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const [params] = useSearchParams();
+  const defaultCompany = params.get("company") || "";
 
-    const [params] = useSearchParams();
-    const defaultCompany = params.get("company") || "";
+  const [selectedCompany, setSelectedCompany] = useState(defaultCompany)
+  const [amount, setAmount] = useState("")
 
-    const [selectedCompany, setSelectedCompany] = useState(defaultCompany);
-    const [amount, setAmount] = useState("");
-
-    const handleDonate = () => {
-        if(!selectedCompany) {
-            showToast("Please select a company to donate to.");
-            return
-        }
-
-        if(!amount){
-            showToast("Please enter an amount")
-            return
-        }
-        
-        if(amount <= 0) {
-            showToast("Please enter a valid donation.");
-            return
-        }
-
-        if(amount > 0) {
-            showToast("Insufficient balance");
-            return
-        }
-
-        const result = donate(selectedCompany, Number(amount))
-
-        if(result.success) {
-            showToast(result.message, "success");
-            
-            setAmount("");
-            setSelectedCompany("");
-            
-            setTimeout(() => {
-                navigate("/");
-            }, 1000);
-        }
+  const handleDonate = () => {
+    if(!selectedCompany) {
+      showToast("Please select a company")
+      return;
     }
+
+    if(!amount) {
+      showToast('Please enter an amount')
+      return
+    }
+
+    if(amount <= 0 ){
+      showToast("Enter a valid donation amount")
+      return
+    }
+
+    if(amount > balance) {
+      showToast("Insufficient balance")
+      return
+    }
+
+    const result = donate(selectedCompany, Number(amount))
+
+    if(result.success) {
+      showToast("Donation Successful", "success")
+
+      setAmount("")
+      setSelectedCompany("")
+
+      setTimeout(() => {
+        navigate("/")
+      }, 1000);
+    }
+  }
 
   return (
     <section className="donation-page">
-        <div className="donation-header">
-            <button className="back-button" onClick={() => navigate('/')}>
-                <i className="bx bx-left-arrow-alt"></i> Back
+      <div className="donation-header">
+        <button className="back-button" onClick={() => navigate('/')}>
+          <i className="bx bx-arrow-left"></i> Back
+        </button>
+
+        <h1 className="donation-title">Make a Donation</h1>
+      </div>
+
+      <div className="donation-card">
+        <i className="bx bx-donate-heart donation-icon"></i>
+
+        <label className="form-label">Select Company</label>
+        <select className="form-select"
+                value={selectedCompany}
+                onChange={(e) => setSelectedCompany(e.target.value)}   >
+          <option value="">Choose a company</option>
+          <option>PayPal</option>
+          <option>Stripe</option>
+          <option>MasterCard</option>
+          <option>Visa</option>
+          <option>Apple Pay</option>
+          <option>Google Pay</option>
+        </select>
+
+        <label className="form-label">Donation Amount</label>
+        <input type="number"
+         className="form-input"
+          placeholder="$ 0.00"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)} />
+
+        <div className="preset-amounts">
+          {[10, 25, 50, 100].map((val) => (
+            <button key={val} className="preser-btn"
+            onClick={() => setAmount(val)}>
+              ${val}
             </button>
-            <h1 className="donation-title">Make a Donation</h1>
-        </div>    
-           
-        <div className="donation-card">
-                <i className="bx bx-heart"></i>
-
-                <label className="form-label">Select Company</label>
-                <select className="form-select"
-                    value={selectedCompany}
-                    onChange={(e) => setSelectedCompany(e.target.value)} >
-                    <option value="">Choose a company</option>
-                    <option>PayPal</option>
-                    <option>Stripe</option>
-                    <option>MasterCard</option>
-                    <option>Visa</option>
-                    <option>Apple Pay</option>
-                    <option>Google Pay</option>
-                </select>
-
-                <label className="form-label">Donation Amount</label>
-                <input type="number" 
-                className="form-input" 
-                placeholder="$ 0.00" 
-                value={amount} 
-                onChange={(e) => setAmount(e.target.value)}/>
-
-                <div className="preset-amounts">{[10,25,50,100].map((val) => 
-                    <button key={val} className="preser-btn"
-                    onClick={() => setAmount(val)}>{val}</button>
-                    )}
-                    </div>
-
-                <button className="donate-submit" onClick={handleDonate}>
-                    <i className="bx bx-heart"></i>Donate Now
-                </button>
+          ))}
         </div>
+
+        <button className="donate-submit" onClick={handleDonate}>
+            <i className="bx bx-heart"></i> Donate Now
+        </button>
+      </div>
     </section>
-  )
+  );
 }
--- practice1/Donate-App/src/pages/HomePage/Balance-Card.jsx
+++ practice1/Donate-App/src/pages/HomePage/Balance-Card.jsx
@@ -2,30 +2,33 @@ import BalanceButton from "./Action-Button";
 import SuggestedCompany from "./Suggested-Companies";
 import '../../styles/Home.css'
 import { useAppContext } from "../../context/AppContext";
+import RecentDonation from "./Recent-Donations";
 
 export default function BalanceCard() {
 
-    const {balance} = useAppContext();
+  const {balance} = useAppContext()
   return (
     <section>
-        <main className="home-page">
-            <div className="balance">
-                <h2>Available on card</h2>
-                <h1>${balance.toLocaleString()}</h1>
+      <main className="home-page">
+        <div className="balance">
+          <h2>Available on card</h2>
+          <h1>${balance.toLocaleString()}</h1>
+        </div>
+        <div className="limit">
+          <span>Transfer Limit</span>
+          <span>$12,000</span>
+        </div>
 
-            </div>
-
-            <div className="limit">
-                <span>Tramsfer Limit</span>
-                <span>$10,000.00</span>
-            </div>
-
-            <div className="balance-btn"><BalanceButton /></div>
-            <div className="suggest-company">
-                <SuggestedCompany />
-            </div>
-            <div className="suggest-company"></div>
+        <div className="balance-btn">
+          <BalanceButton />
+        </div>
+      </main>
+        <main className="suggest-company">
+            <SuggestedCompany />
+        </main>
+        <main className="suggest-company">
+          <RecentDonation />
         </main>
     </section>
   );
-}   
+}
