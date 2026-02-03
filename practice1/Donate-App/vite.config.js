98a299de20b8fa9acb45501ee06e5a5c1b3193f7
rgalarpe <46391075+rgalarpe@users.noreply.github.com>
2026-02-03 06:03:55 +0000

    try github pages

--- practice1/Donate-App/vite.config.js
+++ practice1/Donate-App/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/commit-to-chaos/',
+  base: '/commit-to-chaos/practice1/Donate-App/',
   plugins: [react()],
 })
