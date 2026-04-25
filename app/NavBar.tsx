bf660456bdbf577c3009acbfcfea315bf21d5b39
rgalarpe <rommelc.galarpe@gmail.com>
2026-04-25 23:03:12 +0800

    57.3 04m25d26y google providers

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,9 +3,10 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className='flex bg-neutral p-3'>
+    <div className='flex bg-neutral p-3 space-x-3'>
       <Link href='/' className='mr-5'>Next.js</Link>
       <Link href='/users' className='mr-5'>Users</Link>
+      <Link href='/api/auth/signin' className='mr-5'>Sign In</Link>
     </div>
   )
 }
